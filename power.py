9b5f517526209454ae2e8caf054c8132151def54
ParthD <92713484+cryptpd@users.noreply.github.com>
2021-11-08 16:43:41 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import os
+
+
+def shutdown():
+    return os.system("shutdown /s /t 1")
+
+
+def restart():
+    return os.system("shutdown /r /t 1")
+
+
+def logout():
+    return os.system("shutdown -l")
+
+
+master = Tk()
+
+master.configure(bg='black')
+
+Button(master, text="Shutdown", command=shutdown).grid(row=0)
+Button(master, text="Restart", command=restart).grid(row=1)
+Button(master, text="Log out", command=logout).grid(row=2)
+
+mainloop()
